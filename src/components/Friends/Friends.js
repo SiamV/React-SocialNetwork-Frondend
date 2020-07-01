2ce7112951f3594d987b4ac3c1d791062e5f9639
unknown <avers3452@gmail.com>
2020-06-30 23:39:10 -0500

    add array comp Friends

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -3,16 +3,25 @@ import classes from './Friends.module.css'
 import {NavLink} from "react-router-dom";
 
 const  Friends = () => {
+    let myFriendsData = [
+        {id: 1, name: "Fedor", link: "/fedor"},
+        {id: 2, name: "Nastya", link: "/nastya"},
+        {id: 3, name: "Nikolay", link: "/nikolay"},
+        {id: 4, name: "Max", link: "/max"},
+        {id: 5, name: "Viktor", link: "/viktor"},
+        {id: 6, name: "Anna", link: "/anna"}
+    ]
+
     return (
         <div className={classes.myFriends}>
             <span>Friends:</span>
             <ul>
-                <NavLink to={'/fedor'} className={classes.NavLink}>Fedor</NavLink>
-                <NavLink to={'/nastya'} className={classes.NavLink}>Nastya</NavLink>
-                <NavLink to={'/nikolay'} className={classes.NavLink}>Nikolay</NavLink>
-                <NavLink to={'/max'} className={classes.NavLink}>Max</NavLink>
-                <NavLink to={'/viktor'} className={classes.NavLink}>Viktor</NavLink>
-                <NavLink to={'/anna'} className={classes.NavLink}>Anna</NavLink>
+                <NavLink to={myFriendsData[0].link} className={classes.NavLink}>{myFriendsData[0].name}</NavLink>
+                <NavLink to={myFriendsData[1].link} className={classes.NavLink}>{myFriendsData[1].name}</NavLink>
+                <NavLink to={myFriendsData[2].link} className={classes.NavLink}>{myFriendsData[2].name}</NavLink>
+                <NavLink to={myFriendsData[3].link} className={classes.NavLink}>{myFriendsData[3].name}</NavLink>
+                <NavLink to={myFriendsData[4].link} className={classes.NavLink}>{myFriendsData[4].name}</NavLink>
+                <NavLink to={myFriendsData[5].link} className={classes.NavLink}>{myFriendsData[5].name}</NavLink>
             </ul>
         </div>
     );
