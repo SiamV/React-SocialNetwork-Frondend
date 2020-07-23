9f202c689e755e20738c66ee86129dec9c0e2155
unknown <avers3452@gmail.com>
2020-07-23 18:01:37 -0500

    use componentDidMount and axios

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -6,45 +6,48 @@ import fotoDefault from '../../drawable/avatarDefault.png'
 class Users extends React.Component {
     constructor(props) {
         super(props);
+    }
+
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 console.log(response);
                 this.props.setUsers(response.data.items)
-            });
+            })
     }
 
-    //Заюзаем этот метод в конструкторе и сделаем get API запрос при создании нового объекта (компоненты) без кнопки
-    // getUsers = () => {
-    //     if (this.props.users.length === 0) {
-    //         axios.get('https://social-network.samuraijs.com/api/1.0/users')
-    //             .then(response => {
-    //                 console.log(response);
-    //                 this.props.setUsers(response.data.items)
-    //             })
-    //     }
-    // }
+//Заюзаем этот метод в конструкторе и сделаем get API запрос при создании нового объекта (компоненты) без кнопки
+// getUsers = () => {
+//     if (this.props.users.length === 0) {
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users')
+//             .then(response => {
+//                 console.log(response);
+//                 this.props.setUsers(response.data.items)
+//             })
+//     }
+// }
 
-    //Через dispatch мы получили с сервера такой массив с данными:
-    //  data:
-    //         error: null
-    //   items: Array(10)
-    //              0:
-    //              followed: false
-    //              id: 9475
-    //              name: "esenoukg"
-    //              photos: {small: null, large: null}
-    //              status: null
-    //              uniqueUrlName: null
-    //              __proto__: Object
-    //     1: {name: "esenou", id: 9474, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     2: {name: "Ben", id: 9473, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     3: {name: "LSD", id: 9472, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     4: {name: "fromery", id: 9471, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     5: {name: "VenchasS", id: 9470, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     6: {name: "VTreactJS", id: 9469, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     7: {name: "ArtyWallace", id: 9468, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     8: {name: "Wallace", id: 9467, uniqueUrlName: null, photos: {…}, status: null, …}
-    //     9: {name: "siegheart", id: 9466, uniqueUrlName: null, photos: {…}, status: null, …}
+//Через dispatch мы получили с сервера такой массив с данными:
+//  data:
+//         error: null
+//   items: Array(10)
+//              0:
+//              followed: false
+//              id: 9475
+//              name: "esenoukg"
+//              photos: {small: null, large: null}
+//              status: null
+//              uniqueUrlName: null
+//              __proto__: Object
+//     1: {name: "esenou", id: 9474, uniqueUrlName: null, photos: {…}, status: null, …}
+//     2: {name: "Ben", id: 9473, uniqueUrlName: null, photos: {…}, status: null, …}
+//     3: {name: "LSD", id: 9472, uniqueUrlName: null, photos: {…}, status: null, …}
+//     4: {name: "fromery", id: 9471, uniqueUrlName: null, photos: {…}, status: null, …}
+//     5: {name: "VenchasS", id: 9470, uniqueUrlName: null, photos: {…}, status: null, …}
+//     6: {name: "VTreactJS", id: 9469, uniqueUrlName: null, photos: {…}, status: null, …}
+//     7: {name: "ArtyWallace", id: 9468, uniqueUrlName: null, photos: {…}, status: null, …}
+//     8: {name: "Wallace", id: 9467, uniqueUrlName: null, photos: {…}, status: null, …}
+//     9: {name: "siegheart", id: 9466, uniqueUrlName: null, photos: {…}, status: null, …}
     render() {
         return (
             <div className={classes.users}>
