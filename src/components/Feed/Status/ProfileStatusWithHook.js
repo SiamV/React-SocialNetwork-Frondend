67051c862f5d29c8009b13e81b15a80334f2c233
SiamV <65418069+siamv@users.noreply.github.com>
2020-08-20 13:50:43 -0500

    useEffect in ProfileStatusWithHook

--- src/components/Feed/Status/ProfileStatusWithHook.js
+++ src/components/Feed/Status/ProfileStatusWithHook.js
@@ -1,16 +1,20 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import classes from "./ProfileStatus.module.css"
 
 export const ProfileStatusWithHook = (props) => {
-    const [editMode, setEditMode] = useState (false);
+    const [editMode, setEditMode] = useState(false);
     const [statusLocal, setStatusLocal] = useState(props.status);
+
+    useEffect(() => {
+        setStatusLocal(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
     }
 
     const deactivateEditMode = () => {
-        setEditMode (false)
+        setEditMode(false)
         props.updateStatus(statusLocal);
     }
 
@@ -18,17 +22,18 @@ export const ProfileStatusWithHook = (props) => {
         setStatusLocal(e.currentTarget.value)
     }
 
-        return (
-            <div className={classes.statusBlock}>
-                <span>My status: </span>
-                { (!editMode) &&
-                <div><span onDoubleClick={activateEditMode}>{props.status === '' || props.status === null ? 'dont have status'
-                    : props.status}</span></div>}
-                {(editMode) &&
-                <div><input onChange={onChangeStatusLocal} value={statusLocal}
-                            onBlur={deactivateEditMode} placeholder={'create new status'}/></div>}
-            </div>
-        )
+    return (
+        <div className={classes.statusBlock}>
+            <span>My status: </span>
+            {(!editMode) &&
+            <div><span
+                onDoubleClick={activateEditMode}>{props.status === '' || props.status === null ? 'dont have status'
+                : props.status}</span></div>}
+            {(editMode) &&
+            <div><input onChange={onChangeStatusLocal} value={statusLocal}
+                        onBlur={deactivateEditMode} placeholder={'create new status'} /></div>}
+        </div>
+    )
 }
 
 export default ProfileStatusWithHook;
