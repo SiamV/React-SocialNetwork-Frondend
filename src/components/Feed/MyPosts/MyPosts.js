a27d7dafa1477ed36e6d88c60619ba7523acac32
Vitaly <avers3452@gmail.com>
2020-08-21 12:50:41 -0500

    MyPost with PureComponent. Optimization rendering

--- src/components/Feed/MyPosts/MyPosts.js
+++ src/components/Feed/MyPosts/MyPosts.js
@@ -2,13 +2,10 @@ import React from "react";
 import Post from "./Post/Post";
 import classes from "./MyPosts.module.css"
 
-class MyPosts extends React.Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return nextProps !== this.props || nextState !== this.state
-    }
+class MyPosts extends React.PureComponent {
+
     render() {
         console.log('render')
-
         let postsElements = this.props.myPostsData.map(p => (
             <Post description={p.post} like={p.like} key={p.id} />
         ))
