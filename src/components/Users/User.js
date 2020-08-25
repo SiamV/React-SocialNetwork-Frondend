d5700a944ee6fdb0314a7deb344d24aebbf035c6
Vitaly <avers3452@gmail.com>
2020-08-24 23:10:32 -0500

    create <User /> and refactoring <Users />

---
+++ src/components/Users/User.js
@@ -0,0 +1,33 @@
+import React from "react";
+import fotoDefault from '../../drawable/avatarDefault.png'
+import {NavLink} from "react-router-dom";
+
+const User = (props) => {
+    return (
+        <div>
+                    <NavLink to={'/profile/' + props.user.id}>
+                        <img src={props.user.photos.small
+                        != null ? props.user.photos.small : fotoDefault}
+                             alt={'avatar'} />
+                    </NavLink>
+                    <div>
+                        {props.user.followed
+                            ? <button disabled={props.isButtonDisabling} onClick={() => {
+                                //use thunk
+                                props.unfollowThunkCreator(props.user.id)
+
+                            }}>Unfollow</button>
+                            : <button disabled={props.isButtonDisabling} onClick={() => {
+                                props.followThunkCreator(props.user.id)
+                            }}>Follow</button>
+
+                        }
+                    </div>
+                    <div>{props.user.name}</div>
+                    <div>{'user.location.country'}</div>
+                    <div>{'user.location.city'}</div>
+                </div>
+    );
+}
+
+export default User;
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,8 +1,7 @@
 import React from "react";
 import classes from "./Users.module.css"
-import fotoDefault from '../../drawable/avatarDefault.png'
-import {NavLink} from "react-router-dom";
 import Paginator from "../common/Paginator/Paginator";
+import User from "./User";
 
 let Users = (props) => {
     return (
@@ -11,29 +10,11 @@ let Users = (props) => {
             <Paginator totalCountUsers={props.totalCountUsers} countUsersPage={props.countUsersPage}
                        onPageChange={props.onPageChange} currentPage={props.currentPage} />
 
-            {props.users.map(u => <div key={u.id}>
-                    <NavLink to={'/profile/' + u.id}>
-                        <img src={u.photos.small
-                        != null ? u.photos.small : fotoDefault}
-                             alt={'avatar'} />
-                    </NavLink>
-                    <div>
-                        {u.followed
-                            ? <button disabled={props.isButtonDisabling} onClick={() => {
-                                //use thunk
-                                props.unfollowThunkCreator(u.id)
-
-                            }}>Unfollow</button>
-                            : <button disabled={props.isButtonDisabling} onClick={() => {
-                                props.followThunkCreator(u.id)
-                            }}>Follow</button>
-
-                        }
-                    </div>
-                    <div>{u.name}</div>
-                    <div>{'u.location.country'}</div>
-                    <div>{'u.location.city'}</div>
-                </div>
+            {props.users.map(u => <User user={u}
+                                        key={u.id}
+                                        isButtonDisabling={props.isButtonDisabling}
+                                        unfollowThunkCreator={props.unfollowThunkCreator}
+                                        followThunkCreator={props.followThunkCreator} />
             )}
         </div>
     );
